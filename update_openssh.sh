56957f5b63e713d69a756edda6ff5958f0cf556b
vikashhack <53208523+vikashhack@users.noreply.github.com>
2024-07-04 19:07:43 +0530

    Add files via upload

---
+++ update_openssh.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+# Update package lists
+sudo apt-get update -y
+
+# Upgrade OpenSSH to the latest version
+sudo apt-get install --only-upgrade openssh-server -y
+
+# Restart the SSH service
+sudo systemctl restart ssh
